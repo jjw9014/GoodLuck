316ed0269cae2da0ca91baa9eb4682a9004bc3b7
001470(江建文) <jiangjianwen@fcbox.com>
2020-01-29 16:23:01 +0800

    提交项目代码-替换数据源配置

---
+++ GoodLuck-server/src/main/java/com/help/server/properties/MysqlDatasourceConfig.java
@@ -0,0 +1,56 @@
+package com.help.server.properties;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.boot.autoconfigure.SpringBootVFS;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+/**
+ * mysql数据源配置
+ * @author Liam(003046)
+ * @date 2019/7/10 下午4:48
+ */
+@Configuration
+@MapperScan(basePackages = {"com.help.server.dao"},sqlSessionTemplateRef = "mysqlSqlSessionTemplate")
+public class MysqlDatasourceConfig {
+
+    @Bean(name = "mysqlDatasource")
+    @ConfigurationProperties(prefix="mysql.jdbc")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().type(com.alibaba.druid.pool.DruidDataSource.class).build();
+    }
+
+    @Bean(name = "mysqlSqlSessionFactory")
+    public SqlSessionFactory setSqlSessionFactory(@Qualifier("mysqlDatasource") DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mysqlmappers/*.xml"));
+        bean.setVfs(SpringBootVFS.class);
+        bean.setTypeAliasesPackage("com.help.server.model");
+        return bean.getObject();
+    }
+
+    @Bean(name = "mysqlTransactionManager")
+    public PlatformTransactionManager platformTransactionManager(@Qualifier("mysqlDatasource") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean(name = "mysqlSqlSessionTemplate")
+    @Primary
+    public SqlSessionTemplate sqlSessionTemplate(@Qualifier("mysqlSqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+}
